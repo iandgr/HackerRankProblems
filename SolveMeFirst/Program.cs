4fd5da8c44ee65d11d5a9f959c6f5b2c61213768
Ian Daniel Garay Rodriguez <iandanielgarayrodriguez@outlook.com>
2023-02-08 21:25:29 -0700

    primer ejercicio

---
+++ SolveMeFirst/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Program
+{
+  static int solveMeFirst(int a, int b) => a + b;
+  static void Main(String[] args)
+  {
+    int val1 = Convert.ToInt32(Console.ReadLine());
+    int val2 = Convert.ToInt32(Console.ReadLine());
+    int sum = solveMeFirst(val1, val2);
+    Console.WriteLine(sum);
+  }
+}
