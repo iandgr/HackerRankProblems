12836f05fbe669da88ac29c945b925b8d42f2e29
Ian Daniel Garay Rodriguez <iandanielgarayrodriguez@outlook.com>
2023-02-25 09:54:21 -0700

    ejercicio 25/02/2023

---
+++ Staircase/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+  /*
+   * Complete the 'staircase' function below.
+   *
+   * The function accepts INTEGER n as parameter.
+   */
+
+  public static void staircase(int n)
+  {
+    for (int i = 0; i < n; i++)
+    {
+      for (int j = 0; j < (n - 1 - i); j++)
+      {
+        Console.Write(" ");
+      }
+      for (int k = 0; k < (i + 1); k++)
+      {
+        Console.Write("#");
+      }
+      Console.Write("\n");
+    }
+  }
+
+}
+
+class Solution
+{
+  public static void Main(string[] args)
+  {
+    int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+    Result.staircase(n);
+  }
+}
+
